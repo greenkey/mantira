022a4a762544e84c223b3e4f010e6e6592963f7a
greenkey <greenkey@loman.it>
2016-02-01 14:21:58 +0100

    aplied some style changes (including funcion name refactoring)

--- mantira.py
+++ mantira.py
@@ -6,45 +6,48 @@ from util import confman
 from urllib.parse import urlparse
 from util.mantis import Mantis
 
-def getJiraMantisIssues(jira,mantis,cfg):
-    dt_str = cfg.get('Mantira','last_check')
+
+def get_jira_mantis_issues(jira, mantis, cfg):
+    dt_str = cfg.get('Mantira', 'last_check')
     issues_mj = jira.search_issues('"Mantis ID" is not EMPTY AND updatedDate > "{}"'.format(dt_str), maxResults=None)
 
     for i in issues_mj:
         mantis_id = i.fields.customfield_10605
         if "http://192.168.132.59/mantis/view.php?" in mantis_id:
-            mantis_id = mantis_id[mantis_id.find("?id=")+4:]
+            mantis_id = mantis_id[mantis_id.find("?id=") + 4:]
         print("Jira: {} → Mantis: {}".format(i.key, mantis_id))
 
-def watchAssignedIssues(jira,mantis,cfg):
-    dt_str = cfg.get('Mantira','last_check')
+
+def watch_assigned_issues(jira, mantis, cfg):
+    dt_str = cfg.get('Mantira', 'last_check')
 
     issues = jira.search_issues('assignee = currentUser() AND updatedDate > "{}"'.format(dt_str), maxResults=None)
     for i in issues:
         try:
             parent_issue = jira.issue(i.fields.parent.key)
             if not parent_issue.fields.watches.isWatching:
-                print("Adding watch on {}".format( parent_issue.key ))
-                jira.add_watcher(parent_issue,jira.current_user())
+                print("Adding watch on {}".format(parent_issue.key))
+                jira.add_watcher(parent_issue, jira.current_user())
         except(AttributeError):
-            print("Error, maybe issue {} doesn't have a parent.".format( i.key ))
+            print("Error, maybe issue {} doesn't have a parent.".format(i.key))
 
-def getJiraIncoherents(jira,mantis,cfg):
-    dt_str = cfg.get('Mantira','last_check')
+
+def get_jira_incoherents(jira, mantis, cfg):
+    dt_str = cfg.get('Mantira', 'last_check')
 
     print("Checking updates since {}".format(dt_str))
 
     issues = jira.search_issues('updatedDate > "{}"'.format(dt_str), maxResults=None)
     for i in issues:
         status = i.fields.status.name
-        
+
         if status == 'Closed':
             for subissue in i.fields.subtasks:
                 if subissue.fields.status.name != "Closed":
                     print("Issue {} is Closed but has at least a subtask not Closed ({})".format(
                         i.key, subissue.key))
 
-        elif status in ('Testing','Developed'):
+        elif status in ('Testing', 'Developed'):
             for subissue in i.fields.subtasks:
                 issuetype = subissue.fields.issuetype.name
                 if issuetype in ('Dev Task'):
@@ -60,7 +63,7 @@ def getJiraIncoherents(jira,mantis,cfg):
                     devstatuses.add(subissue.fields.status.name)
             if len(devstatuses) == 1 and 'In Progress' not in devstatuses:
                 print("Issue {} is In Progress but has all the Dev Task {}".format(
-                            i.key, devstatuses))
+                    i.key, devstatuses))
 
         elif status == 'Open':
             for subissue in i.fields.subtasks:
@@ -71,57 +74,63 @@ def getJiraIncoherents(jira,mantis,cfg):
         else:
             pass
 
-def getJiraMantisIncoherents(jira,mantis,cfg):
-    dt_str = cfg.get('Mantira','last_check')
+
+def get_jira_mantis_incoherents(jira, mantis, cfg):
+    dt_str = cfg.get('Mantira', 'last_check')
     issues_mj = jira.search_issues('"Mantis ID" is not EMPTY AND updatedDate > "{}"'.format(dt_str), maxResults=None)
 
     for ji in issues_mj:
         mantis_id = ji.fields.customfield_10605
         if "http://192.168.132.59/mantis/view.php?" in mantis_id:
-            mantis_id = mantis_id[mantis_id.find("?id=")+4:]
+            mantis_id = mantis_id[mantis_id.find("?id=") + 4:]
         try:
             mi = mantis.mc_issue_get(issue_id=mantis_id)
         except:
-            #print('Error getting issue "{}" on Mantis'.format(mantis_id))
+            # print('Error getting issue "{}" on Mantis'.format(mantis_id))
             continue
-        
+
         j_status = ji.fields.status.name
         m_status = mi.status.name
 
-        if (j_status == 'Open' and m_status not in ('new', 'reopen')) or (j_status == 'In Progress' and m_status not in ('feedback', 'acknoweledged', 'confirmed', 'assigned')) or (j_status == 'Closed' and m_status not in ('resolved', 'closed', 'rejected')):
+        if (j_status == 'Open' and m_status not in ('new', 'reopen')) or (
+                j_status == 'In Progress' and m_status not in (
+        'feedback', 'acknoweledged', 'confirmed', 'assigned')) or (
+                j_status == 'Closed' and m_status not in ('resolved', 'closed', 'rejected')):
             print('Issue {} on Mantis is {} but the linked {} on Jira is {}'.format(
                 mi.id, m_status, ji.key, j_status
-                ))
+            ))
 
 
-def getAllIncoherents(jira,mantis,cfg):
-    getJiraIncoherents(jira,mantis,cfg)
-    getJiraMantisIncoherents(jira,mantis,cfg)
+def get_all_incoherents(jira, mantis, cfg):
+    get_jira_incoherents(jira, mantis, cfg)
+    get_jira_mantis_incoherents(jira, mantis, cfg)
+
 
 if __name__ == '__main__':
 
     # first get the parameters of the script
     parser = argparse.ArgumentParser(description='Get the Jira issues linked to Mantis.')
     parser.add_argument('action', metavar='ACTION', type=str,
-                       help='the action to be performed')
+                        help='the action to be performed')
     parser.add_argument('--config', dest='config_file',
-                       action='store', default='.mantira_config.json',
-                       help='Set the config file to use '
-                            '(default: .mantira_config.json)')
+                        action='store', default='.mantira_config.json',
+                        help='Set the config file to use '
+                             '(default: .mantira_config.json)')
     parser.add_argument('--from_date', dest='from_date',
-                       action='store', default='last_check',
-                       help='From which date the analysis should take place, ISO format '
-                            '(default = last check)')
+                        action='store', default='last_check',
+                        help='From which date the analysis should take place, ISO format '
+                             '(default = last check)')
     args = parser.parse_args()
 
     available_actions = {
-        "get_jira_issues": getJiraMantisIssues,
-        "watch_assigned_issues": watchAssignedIssues,
-        "incoherents": getJiraIncoherents,
-        "allincoherents": getAllIncoherents
+        "get_jira_issues": get_jira_mantis_issues,
+        "watch_assigned_issues": watch_assigned_issues,
+        "incoherents": get_jira_incoherents,
+        "allincoherents": get_all_incoherents
     }
     if args.action not in available_actions:
-        print("Action {} not available.\nList of available actions: {}".format(args.action,list(available_actions.keys())))
+        print("Action {} not available.\nList of available actions: {}".format(args.action,
+                                                                               list(available_actions.keys())))
         exit()
 
     # get configuration
@@ -129,27 +138,28 @@ if __name__ == '__main__':
 
     # connect to Jira
     jira = JIRA(
-        cfg.get('Jira','host','Insert the url of jira (i.e. http://jira.yourdomain.com): '),
+        cfg.get('Jira', 'host', 'Insert the url of jira (i.e. http://jira.yourdomain.com): '),
         basic_auth=(
-            cfg.get('Jira','username','Insert the username for Jira: '),
-            cfg.get('Jira','password','Insert the username for Jira (it will be'
-                                         ' saved in the config file: '+cfg.cfgFile+'): ')))
+            cfg.get('Jira', 'username', 'Insert the username for Jira: '),
+            cfg.get('Jira', 'password', 'Insert the username for Jira (it will be'
+                                        ' saved in the config file: ' + cfg.cfgFile + '): ')))
 
     mantis = Mantis(
-            cfg.get('Mantis','wsdl','Insert the wsdl of Mantis WS: '),
-            cfg.get('Mantis','username','Insert username for Mantis: '),
-            cfg.get('Mantis','password','Insert password for Mantis: (it will be'
-                                         ' saved in the config file: '+cfg.cfgFile+'): '))
+        cfg.get('Mantis', 'wsdl', 'Insert the wsdl of Mantis WS: '),
+        cfg.get('Mantis', 'username', 'Insert username for Mantis: '),
+        cfg.get('Mantis', 'password', 'Insert password for Mantis: (it will be'
+                                      ' saved in the config file: ' + cfg.cfgFile + '): '))
 
-    if(args.from_date == 'last_check'):
-        dt_str = cfg.get('Mantira','last_check','Check never performed, insert a date from which filter the issues (YYYY-MM-DD): ')
+    if (args.from_date == 'last_check'):
+        dt_str = cfg.get('Mantira', 'last_check',
+                         'Check never performed, insert a date from which filter the issues (YYYY-MM-DD): ')
     else:
         dt_str = args.from_date
     cfg.put('Mantira', 'last_check', dt_str)
 
-    available_actions[args.action](jira=jira,mantis=mantis,cfg=cfg)
+    available_actions[args.action](jira=jira, mantis=mantis, cfg=cfg)
 
     from datetime import date, timedelta
+
     yesterday = date.today() - timedelta(1)
     cfg.put('Mantira', 'last_check', yesterday.strftime("%Y-%m-%d"))
-
